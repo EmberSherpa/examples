1945bf808198d9ad1aec54bec9759ebb87c57244
Taras Mankovski <tarasm@gmail.com>
2013-12-27 08:42:04 +0100

    Added tests

--- app/helpers/github-link.js
+++ app/helpers/github-link.js
@@ -1,4 +1,4 @@
 export default Ember.Handlebars.makeBoundHelper(function(path) {
-  var result = '<a target="_blank" href="https://github.com/embersherpa/examples/blob/master/app/%@">%@</a>'.fmt(path, path);
+  var result = '<a target="_blank" href="https://github.com/embersherpa/examples/blob/master/%@">%@</a>'.fmt(path, path);
   return new Ember.Handlebars.SafeString(result);
 });
