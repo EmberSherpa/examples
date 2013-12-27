305f716c4a84a5787faae06a72a99c5374aac976
Taras Mankovski <tarasm@gmail.com>
2013-12-27 08:37:17 +0100

    Fixed jshint Handlebars problem Fixed path to deck.js

--- app/helpers/github-link.js
+++ app/helpers/github-link.js
@@ -1,4 +1,4 @@
 export default Ember.Handlebars.makeBoundHelper(function(path) {
   var result = '<a target="_blank" href="https://github.com/embersherpa/examples/blob/master/app/%@">%@</a>'.fmt(path, path);
-  return new Handlebars.SafeString(result);
+  return new Ember.Handlebars.SafeString(result);
 });
